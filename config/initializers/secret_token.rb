eac017602f7664202271216d4ce6744ad15efbc0
patbenatar <nick@gophilosophie.com>
2012-07-26 10:56:33 -0700

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotationalMomentum::Application.config.secret_token = '8bf23ed5f3a8bb52d5d31e2a8b76ab92704cdb06fb0c49a234492c98cc03c47791f62e7e97e0165d16d39bd593880334c1bb6c0eba397f0f4d2bff2134e02d8e'
